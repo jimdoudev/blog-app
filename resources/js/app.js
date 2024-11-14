0ce2ed7d0af633e75e2cc8f87a284c6ccc184c57
Dimitrios Doumpis <doumpis@hotmail.com>
2024-11-15 01:08:50 +0200

    Implements homepage

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+import "./bootstrap";
+
+const menuButton = document.querySelector("#menu-button");
+const closeMenuButton = document.querySelector("#close-menu-button");
+const mobileMenu = document.querySelector("#mobile-menu");
+menuButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    mobileMenu.classList.toggle("hidden");
+});
+closeMenuButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    mobileMenu.classList.toggle("hidden");
+});
